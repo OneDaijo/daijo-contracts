8f6e6e0a0a8f952c21c3c22e41553031d31822f9
Tom Dudzik <tdudz97@gmail.com>
2017-08-04 16:00:59 -0700

    change tests for initial QIN token contract

--- test/qintoken.js
+++ test/qintoken.js
@@ -1,8 +1,8 @@
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var QINToken = artifacts.require("./QINToken.sol");
 
-contract('MetaCoin', function(accounts) {
-  it("should put 10000 MetaCoin in the first account", function() {
-    return MetaCoin.deployed().then(function(instance) {
+contract('QINToken', function(accounts) {
+  it("should put 10000 QIN in the first account", function() {
+    return QINToken.deployed().then(function(instance) {
       return instance.getBalance.call(accounts[0]);
     }).then(function(balance) {
       assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
