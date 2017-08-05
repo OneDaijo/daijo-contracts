5dddd44ef8e5eb0b8cc4a7689ba596872ba010aa
Matt Rice <mrice@worldrapidfinance.com>
2017-08-05 14:05:33 -0400

    WIP simple qin purchase method

--- contracts/ConvertLib.sol
+++ contracts/ConvertLib.sol
@@ -9,4 +9,19 @@ library ConvertLib {
 	{
 		return amount * conversionRate;
 	}
+
+	// Returns USD/10^18
+	function ethToUSD(uint input_wei) returns (uint usdEquivalent) {
+		// Not sure why the line below causes an error.  Not sure how to do library self-reference...
+		// return ConvertLib.convert(input_wei, 1);
+		return input_wei;
+	}
+
+	// Inputs input_wei - amount of eth in amount_wei
+	// This function returns values in QIN.
+	function ethToQIN(uint input_wei) returns (uint qinEquivalent) {
+		// Same as above.
+		// return ConvertLib.ethToUSD(input_wei);
+		return input_wei;
+	}
 }
