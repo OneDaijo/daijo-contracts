ec55d57d893e4329267c20e7423d93ea8b3d2499
Tom Dudzik <tdudz97@gmail.com>
2017-08-04 17:46:47 -0700

    make this thing actually build lol

--- contracts/QINToken.sol
+++ contracts/QINToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.15;
+pragma solidity ^0.4.13;
 
 import "./ConvertLib.sol";
 import "./ERC20Token.sol";
@@ -9,40 +9,40 @@ import "./ERC20Token.sol";
  */
 contract QINToken is ERC20Token {
 
-	string public name = "QIN Token";
-	string public symbol = "QIN";
-	uint public decimals = 5;
-	totalSupply = 200000000;
+    string public name = "QIN Token";
+    string public symbol = "QIN";
+    uint public decimals = 5;
 
-	// crowdsale info
-	uint public startBlock;
-	uint public endBlock;
-	uint public crowdsaleTokenSupply;
-	address public wallet; // address where raised ETH will be deposited
-	bool public halted = false; // for crowdsale emergencies
+    // crowdsale info
+    uint public startBlock;
+    uint public endBlock;
+    uint public crowdsaleTokenSupply;
+    address public wallet; // address where raised ETH will be deposited
+    bool public halted = false; // for crowdsale emergencies
 
-	// TODO vars for allocations + locktimes?
+    // TODO vars for allocations + locktimes?
 
-	function QINToken(address _wallet, uint _startBlock, uint _endBlock) {
-		wallet = _wallet;
-		startBlock = _startBlock;
-		endBlock = _endBlock;
-	}
+    function QINToken(address _wallet, uint _startBlock, uint _endBlock) {
+        totalSupply = 200000000;
+        wallet = _wallet;
+        startBlock = _startBlock;
+        endBlock = _endBlock;
+    }
 
-	function getBalanceInUSD(address addr) returns(uint){
-		return ConvertLib.convert(balanceOf(addr), 2); // TODO figure out conversion rate here
-	}
+    function getBalanceInUSD(address addr) returns(uint){
+        return ConvertLib.convert(balanceOf(addr), 2); // TODO figure out conversion rate here
+    }
 
-	// functions to halt and unhalt crowdsale in case of emergency
-	function haltCrowdsale() {
-        if (msg.sender != founder) 
-        	throw;
+    // functions to halt and unhalt crowdsale in case of emergency
+    function haltCrowdsale() {
+        if (msg.sender != wallet) 
+            throw;
         halted = true;
     }
 
     function unhaltCrowdsale() {
-        if (msg.sender != founder)
-        	throw;
+        if (msg.sender != wallet)
+            throw;
         halted = false;
     }
 }
