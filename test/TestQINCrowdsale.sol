17a202a53e5e8e7c2fc9ec41052b888a26dbe33b
Nicholas Deng <nick.deng@crfanalytics.com>
2017-08-17 00:46:12 -0400

    small progress in test coverage for crowdsale and qinfrozen

--- test/TestQINCrowdsale.sol
+++ test/TestQINCrowdsale.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.13;
 
 import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
 import "../contracts/crowdsale/QINCrowdsale.sol";
 
 /** @title QIN Crowdsale Tests
@@ -13,5 +14,15 @@ contract TestQINCrowdsale {
         uint endBlock = block.number + 5;
 		QINCrowdsale tcs = new QINCrowdsale(startBlock, endBlock, 10, 0x1234);
 		Assert.equal(tcs.startBlock(), startBlock, "Incorrect startblock.");
+		Assert.equal(tcs.endBlock(), endBlock, "Incorrect endblock.");
+		Assert.equal(tcs.rate(), 10, "Incorrect rate.");
+		Assert.equal(tcs.wallet(), 0x1234, "Incorrect waller address.");
 	}
+
+	//  function testBuyQINTokens() {
+	//	  uint startBlock = block.number + 1;
+    //    uint endBlock = block.number + 5;
+	//	  INCrowdsale tcs = QINCrowdsale(startBlock, endBlock, 10, 0x1234);
+	// }
+
 }
