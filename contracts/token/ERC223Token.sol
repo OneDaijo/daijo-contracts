924876fb108bd8bd185ed73899044a5a3c2048cc
Nick Deng <30166427+nickdeng4@users.noreply.github.com>
2017-08-17 15:19:44 -0400

    test commit
    separated crowdsale tests
    
    flattened crowdsale into QINToken and removed startCrowdsale, created Haltable
    
    solved previous test, added new test
    
    testCorrectBalancesAfterTransferringFromAnotherAccount() is more or less correct, simply requires transferFrom() to be run from 0x5678 rather than the owner.
    
    reflect small changes from merged PR
    
    synced with master structure, modified some testing

--- contracts/token/ERC223Token.sol
+++ contracts/token/ERC223Token.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.13;
 
-import "./interfaces/ERC223.sol";
+import "./interfaces/ERC223Interface.sol";
 import "./ERC20Token.sol";
 import "./interfaces/ERC223ReceivingContract.sol";
 import "../libs/SafeMath.sol";
@@ -9,11 +9,26 @@ import "../libs/SafeMath.sol";
  *  @author WorldRapidFinance <info@worldrapidfinance.com>
  *  @notice source: https://github.com/ethereum/EIPs/issues/223
  */
-contract ERC223Token is ERC223, ERC20Token {
+contract ERC223Token is ERC223Interface, ERC20Token {
+    using SafeMath for uint256;
+
+    // assemble the given address bytecode. If bytecode exists then the _addr is a contract.
+    function isContract(address _to) private returns (bool isContract) {
+        uint length;
+        assembly {
+            //retrieve the size of the code on target address, this needs assembly
+            length := extcodesize(_to)
+        }
+        if (length > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
    
     // note: overrides the transfer function in ERC20Token
     //       included for backwards compatibility
-    function transfer(address _to, uint _value) returns (bool success) { 
+    function transfer(address _to, uint256 _value) returns (bool success) { 
         bytes memory empty;
         if(isContract(_to)) {
             return transferToContract(_to, _value, empty);
@@ -30,14 +45,16 @@ contract ERC223Token is ERC223, ERC20Token {
         }   
     }
 
-    // assemble the given address bytecode. If bytecode exists then the _addr is a contract.
-    function isContract(address _addr) private returns (bool isContract) {
-        uint length;
-        assembly {
-            //retrieve the size of the code on target address, this needs assembly
-            length := extcodesize(_addr)
-        }
-        return length > 0;
+    // function that is called when transaction target is a contract
+    function transferToContract(address _to, uint _value, bytes _data) private returns (bool success) {
+        require(balanceOf(msg.sender) >= _value);
+
+        balances[msg.sender] = balanceOf(msg.sender).sub(_value);
+        balances[_to] = balanceOf(_to).add(_value);
+        //ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
+        //receiver.tokenFallback(msg.sender, _value, _data);
+        Transfer(msg.sender, _to, _value, _data);
+        return true;
     }
 
     // function that is called when transaction target is an address
@@ -50,15 +67,5 @@ contract ERC223Token is ERC223, ERC20Token {
         return true;
     }
   
-    // function that is called when transaction target is a contract
-    function transferToContract(address _to, uint _value, bytes _data) private returns (bool success) {
-        require(balanceOf(msg.sender) >= _value);
 
-        balances[msg.sender] = balanceOf(msg.sender).sub(_value);
-        balances[_to] = balanceOf(_to).add(_value);
-        ERC223ReceivingContract receiver = ERC223ReceivingContract(_to);
-        receiver.tokenFallback(msg.sender, _value, _data);
-        Transfer(msg.sender, _to, _value, _data);
-        return true;
-    }
 }
