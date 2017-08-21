cc86dfc8c926d329322839501d0f9bdb2f84f978
Matt Rice <mrice@worldrapidfinance.com>
2017-08-20 21:57:51 -0400

    various corrections and expanded the integration test a bit

--- test/TestQINFrozen.sol
+++ test/TestQINFrozen.sol
@@ -3,19 +3,19 @@ pragma solidity ^0.4.13;
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/token/QINFrozen.sol";
+import "../contracts/token/QINToken.sol";
 
 /** @title Frozen QIN Tests
  *  @author WorldRapidFinance <info@worldrapidfinance.com>
  */
-contract TestQINFrozen {
+contract TestQINFrozen is QINToken {
 
     function testQINFrozenInit() {
         uint releaseTime = now + 1000;
-        uint frozenBalance = 20000;
-        QINFrozen freeze = new QINFrozen(releaseTime, frozenBalance);
+        QINFrozen freeze = new QINFrozen(releaseTime);
 
         Assert.equal(freeze.releaseTime(), releaseTime, "Incorrect release time.");
-        Assert.equal(freeze.frozenBalance(), frozenBalance, "Incorrect frozen balance.");
+        Assert.equal(freeze.frozenBalance(), 0, "Incorrect frozen balance.");
     }
 
 }
