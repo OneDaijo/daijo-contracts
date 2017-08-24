e730f81683511fd3f0917004704db7f9fee6ecb3
Matt Rice <mrice@worldrapidfinance.com>
2017-08-24 00:28:21 -0400

    formatting fix

--- contracts/token/QINToken.sol
+++ contracts/token/QINToken.sol
@@ -36,7 +36,7 @@ contract QINToken is ERC223Token, Ownable {
     }
 
     function startCrowdsale(uint256 _startBlock, uint256 _endBlock, uint256 _rate, address _wallet, uint _releaseTime) onlyOwner {
-    	require(!crowdsaleExecuted);
+        require(!crowdsaleExecuted);
         crowdsale = new QINCrowdsale(this, _startBlock, _endBlock, _rate, _wallet);
 
         // Must transfer ownership to the owner of the QINToken contract rather than the QINToken itself.
