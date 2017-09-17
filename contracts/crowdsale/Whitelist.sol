a3b283537092956cb27c37b6f74e9addd7bf220b
Zane Grube <zlgrube@gmail.com>
2017-09-16 20:53:19 -0400

    Revert "Typo cleanup"
    This reverts commit 74a257f4f97ce268d5e71fbe7db3e8334dd85996.

--- contracts/crowdsale/Whitelist.sol
+++ contracts/crowdsale/Whitelist.sol
@@ -12,21 +12,20 @@ contract whitelist {
     // Number of addresses on the whitelist
     uint public registeredUserCount = 0;
 
-    mapping(address => bool) registeredUserWhitelist;
 
-    // Adds an address to the whitelist
+    //adds an address to the whitelist
     function addToWhitelist(address _addr) external onlyOwner {
       registeredUserWhitelist[_addr] = true;
       registeredUserCount = registeredUserCount.add(1);
     }
 
-    // Removes an address from the whitelist
+    //removes an address from the whitelist
     function removeFromWhitelist(address _addr)  external onlyOwner {
       registeredUserWhitelist[_addr] = false;
       registeredUserCount = registeredUserCount.sub(1);
     }
 
-    // Returns true if address is on the whitelist
+    //returns true if address is on the whitelist
     function getUserRegistrationState(address _addr) public constant returns (bool) {
       return registeredUserWhitelist[_addr];
     }
