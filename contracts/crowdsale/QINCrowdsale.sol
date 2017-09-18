9db7f9d98bec578aae1a98e5a7f43036e88928e1
nickdeng4 <nick.deng@crfanalytics.com>
2017-09-18 15:38:53 -0400

    Merge branch 'master' into crowdsale_update

--- contracts/crowdsale/QINCrowdsale.sol
+++ contracts/crowdsale/QINCrowdsale.sol
@@ -77,7 +77,6 @@ contract QINCrowdsale is ERC223ReceivingContract, Haltable {
         require(_rate > 0);
         require(_wallet != 0x0);
 
-        // TODO(mrice) assumes the QINToken is the creator. If not, we should take the QIN token in explicitly.
         token = _token;
         startTime = _startTime;
         dailyReset = _startTime;
--- truffle.js
+++ truffle.js
@@ -5,10 +5,10 @@ module.exports = {
       		port: 8545,
       		network_id: "*" // Match any network id
     	},
-			test: {
-				host: "localhost",
-				port: 8545,
-				network_id: "*" // Match any network id
-			}
+        test: {
+                host: "localhost",
+                port: 8545,
+                network_id: "*" // Match any network id
+        }
   	}
 };
