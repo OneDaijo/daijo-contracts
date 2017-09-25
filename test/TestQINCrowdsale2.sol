c617fbfbb00248371df2d79c9e82f44e3c96cc93
Zane Grube <zlgrube@gmail.com>
2017-09-24 21:39:36 -0400

    Manual merge (one more thing)

--- test/TestQINCrowdsale2.sol
+++ test/TestQINCrowdsale2.sol
@@ -8,53 +8,6 @@ import "../contracts/crowdsale/QINCrowdsale.sol";
 
 contract TestQINCrowdsale2 {
 
-<<<<<<< HEAD
-  function testUserCountPostRegistration() {
-    uint startTime = now + 100;
-		uint endTime = now + 200;
-		address wallet = 0x1234;
-		uint restrictedDays = 3;
-		QINToken qin = new QINToken();
-		QINCrowdsale tcs = new QINCrowdsale(qin, startTime, endTime, restrictedDays, 10, wallet);
-
-    uint userCountBefore = tcs.registeredUserCount();
-    tcs.addToWhitelist(wallet);
-    uint userCountAfter = tcs.registeredUserCount();
-    Assert.equal(userCountBefore, 0, "Incorrect original user count.");
-    Assert.equal(userCountAfter, 1, "Incorrect new user count.");
-	}
-
-  function testGetUserRegistration() {
-    uint startTime = now + 100;
-		uint endTime = now + 200;
-		address wallet = 0x1234;
-    address wallet2 = 0x5678;
-		uint restrictedDays = 3;
-		QINToken qin = new QINToken();
-		QINCrowdsale tcs = new QINCrowdsale(qin, startTime, endTime, restrictedDays, 10, wallet);
-
-    tcs.addToWhitelist(wallet);
-
-    bool walletStatus = tcs.getUserRegistrationState(wallet);
-    bool walletStatus2 = tcs.getUserRegistrationState(wallet2);
-
-    Assert.equal(walletStatus, true, "User was not registered.");
-    Assert.equal(walletStatus2, false, "User was registered somehow");
-  }
-
-  function testSetRestrictedDays() {
-    uint startTime = now + 100;
-		uint endTime = now + 200;
-		address wallet = 0x1234;
-		uint restrictedDays = 3;
-		QINToken qin = new QINToken();
-		QINCrowdsale tcs = new QINCrowdsale(qin, startTime, endTime, restrictedDays, 10, wallet);
-
-    Assert.equal(tcs.numRestrictedDays(), 3, "Incorrect initial number of restricted days.");
-		tcs.setRestrictedSaleDays(5);
-		Assert.equal(tcs.numRestrictedDays(), 5, "Incorrect modified number of restricted days.");
-	}
-=======
     function testUserCountPostRegistration() {
         uint startTime = now + 100;
         uint endTime = now + 200;
@@ -100,5 +53,4 @@ contract TestQINCrowdsale2 {
         tcs.setRestrictedSaleDays(5);
         Assert.equal(tcs.numRestrictedDays(), 5, "Incorrect modified number of restricted days.");
     }
->>>>>>> master
 }
