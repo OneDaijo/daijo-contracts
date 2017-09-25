7802a537985b1a92cee15fdf2f31779dbb846634
Matt Rice <30190337+thiefinparis@users.noreply.github.com>
2017-09-24 21:09:00 -0400

    Cleanup style, fix test, and add linter to CI (#39)

--- contracts/token/interfaces/ERC20Interface.sol
+++ contracts/token/interfaces/ERC20Interface.sol
@@ -1,19 +1,20 @@
 pragma solidity ^0.4.13;
 
+
 /** @title ERC20 Interface
  *  @author WorldRapidFinance <info@worldrapidfinance.com>
  *  @notice source: https://github.com/ethereum/EIPs/issues/20
  */
 contract ERC20Interface {
 
-    function totalSupply() constant returns (uint256 totalSupply);
-    function balanceOf(address _owner) constant returns (uint256 balance);
-    function transfer(address _to, uint256 _value) public returns (bool success);
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-    function approve(address _spender, uint256 _value) returns (bool success);
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+    function totalSupply() constant returns (uint);
+    function balanceOf(address _owner) constant returns (uint);
+    function transfer(address _to, uint _value) public returns (bool);
+    function transferFrom(address _from, address _to, uint _value) returns (bool);
+    function approve(address _spender, uint _value) returns (bool);
+    function allowance(address _owner, address _spender) constant returns (uint);
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Transfer(address indexed _from, address indexed _to, uint _value);
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
 
 }
