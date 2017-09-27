45fd1c4847ed56d20ea094a3e953b3aadecdd65f
nickdeng4 <nick.deng@crfanalytics.com>
2017-09-27 15:22:11 -0400

    fixed commenting

--- test/TestQINFrozen.sol
+++ test/TestQINFrozen.sol
@@ -10,10 +10,10 @@ import "../contracts/libs/SafeMath.sol";
 /** @title Frozen QIN Tests
  *  @author WorldRapidFinance <info@worldrapidfinance.com>
  */
-/*contract TestQINFrozen {
-    using SafeMath for uint;
+contract TestQINFrozen {
+    /*using SafeMath for uint;
 
-    uint decimalMultiplier = 10**18;
+    uint decimalMultiplier = 10**18;*/
 
     // TODO(thiefinparis): testing these internal functions (if we even want to do this) will requre special
     // test contracts to provide hooks into these internal methods.
@@ -40,7 +40,7 @@ import "../contracts/libs/SafeMath.sol";
     //     Assert.equal(qin.balanceOf(qin.owner()), 0, "Incorrect amount of QIN sent.");
     // }
 
-    function testQINFrozenInit() {
+    /*function testQINFrozenInit() {
         uint releaseTime = now + 1000;
 
         QINToken qin = new QINToken();
@@ -49,9 +49,9 @@ import "../contracts/libs/SafeMath.sol";
 
         Assert.equal(freeze.releaseTime(), releaseTime, "Incorrect release time.");
         Assert.equal(freeze.frozenBalance(), 0, "Incorrect frozen balance.");
-    }
+    }*/
 
-    function testTransferToFrozenQIN() {
+    /*function testTransferToFrozenQIN() {
         uint releaseTime = now + 1000;
         QINToken qin = new QINToken();
         QINFrozen freeze = new QINFrozen(qin, releaseTime);
@@ -61,17 +61,17 @@ import "../contracts/libs/SafeMath.sol";
         Assert.isTrue(freeze.frozen(), "Not frozen.");
         Assert.equal(qin.balanceOf(this), decimalMultiplier.mul(60000000), "Incorrect.");
         Assert.equal(qin.balanceOf(freeze), decimalMultiplier.mul(140000000), "Incorrect.");
-    }
+    }*/
 
-    function testQINFrozenOwner() {
+    /*function testQINFrozenOwner() {
         uint releaseTime = now + 1000;
         QINToken qin = new QINToken();
         QINFrozen freeze = new QINFrozen(qin, releaseTime);
 
         Assert.equal(freeze.owner(), this, "Not the correct owner. ");
-    }
+    }*/
 
-    function testQINFrozenSupportsToken() {
+    /*function testQINFrozenSupportsToken() {
         uint releaseTime = now + 1000;
         QINToken qin = new QINToken();
         QINFrozen freeze = new QINFrozen(qin, releaseTime);
@@ -79,7 +79,7 @@ import "../contracts/libs/SafeMath.sol";
         bool support = freeze.supportsToken(qin);
 
         Assert.equal(support, true, "supportsToken() is rejecting QIN.");
-    }
+    }*/
 
     // TODO(thiefinparis): Like the tests at the top, triggering the release will require hooks into
     // the internal contract state to modify the time to allow the payment and release to occur in
@@ -98,4 +98,4 @@ import "../contracts/libs/SafeMath.sol";
 
     //}
 
-}*/
+}
