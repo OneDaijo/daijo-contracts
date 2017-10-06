d2848aeeffd7e917637d2f30b755f9ecc991881d
nickdeng4 <nick.deng@crfanalytics.com>
2017-10-06 16:19:50 -0400

    uncommented and modified QINFrozen and related tests.

--- test/TestQINFrozen.sol
+++ test/TestQINFrozen.sol
@@ -18,84 +18,86 @@ contract TestQINFrozen {
     // TODO(thiefinparis): testing these internal functions (if we even want to do this) will requre special
     // test contracts to provide hooks into these internal methods.
 
-    // // Note: freezeRemainingTokens() function is private, remove modifier temporarily to perform test
-    // function testQINFrozenInitFromQINToken() {
-    //     uint releaseTime = now + 1000;
-    //     uint freezeAmount = decimalMultiplier.mul(200000000);
-    //     QINToken qin = new QINToken(true);
-    //     qin.freezeRemainingTokens(releaseTime, freezeAmount);
-    //     address expected = this;
-    //     address owner = qin.getQINFrozen().owner();
+    // Note: freezeRemainingTokens() function is private, remove modifier temporarily to perform test
+    // Note: the following two tests are disabled because freezeRemainingTokens() no longer exists
+    // in QINToken.
+    /*function testQINFrozenInitFromQINToken() {
+        uint releaseTime = now + 1000;
+        uint freezeAmount = decimalMultiplier.mul(200000000);
+        QINToken qin = new QINToken(true);
+        qin.freezeRemainingTokens(releaseTime, freezeAmount);
+        address expected = this;
+        address owner = qin.getQINFrozen().owner();
 
-    //     Assert.equal(owner, expected, "Freeze has wrong owner.");
-    // }
+        Assert.equal(owner, expected, "Freeze has wrong owner.");
+    }
 
-    // // Note: freezeRemainingTokens() function is private, remove modifier temporarily to perform test
-    // function testQINTokenIsTransferringToQINFrozen() {
-    //     uint releaseTime = now + 1000;
-    //     uint freezeAmount = decimalMultiplier.mul(200000000);
-    //     QINToken qin = new QINToken(true);
-    //     qin.freezeRemainingTokens(releaseTime, freezeAmount);
+    //  Note: freezeRemainingTokens() function is private, remove modifier temporarily to perform test
+    function testQINTokenIsTransferringToQINFrozen() {
+        uint releaseTime = now + 1000;
+        uint freezeAmount = decimalMultiplier.mul(200000000);
+        QINToken qin = new QINToken(true);
+        qin.freezeRemainingTokens(releaseTime, freezeAmount);
 
-    //     Assert.equal(qin.balanceOf(qin.owner()), 0, "Incorrect amount of QIN sent.");
-    // }
+        Assert.equal(qin.balanceOf(qin.owner()), 0, "Incorrect amount of QIN sent.");
+    }*/
 
-    //function testQINFrozenInit() {
-    //    uint releaseTime = now + 1000;
+    function testQINFrozenInit() {
+        uint releaseTime = now + 1000;
 
-    //    QINToken qin = new QINToken(true);
+        QINToken qin = new QINToken(true);
 
-    //    QINFrozen freeze = new QINFrozen(qin, releaseTime);
+        QINFrozen freeze = new QINFrozen(qin, releaseTime, true);
 
-    //    Assert.equal(freeze.releaseTime(), releaseTime, "Incorrect release time.");
-    //    Assert.equal(freeze.frozenBalance(), 0, "Incorrect frozen balance.");
-    //}
+        Assert.equal(freeze.releaseTime(), releaseTime, "Incorrect release time.");
+        Assert.equal(freeze.frozenBalance(), 0, "Incorrect frozen balance.");
+    }
 
-    //function testTransferToFrozenQIN() {
-    //    uint releaseTime = now + 1000;
-    //    QINToken qin = new QINToken(true);
-    //    QINFrozen freeze = new QINFrozen(qin, releaseTime);
+    function testTransferToFrozenQIN() {
+        uint releaseTime = now + 1000;
+        QINToken qin = new QINToken(true);
+        QINFrozen freeze = new QINFrozen(qin, releaseTime, true);
 
-    //    Assert.isFalse(freeze.frozen(), "Frozen before payment.");
-    //    Assert.isTrue(qin.transfer(freeze, decimalMultiplier.mul(140000000)), "Transfer to freeze failed.");
-    //    Assert.isTrue(freeze.frozen(), "Not frozen.");
-    //    Assert.equal(qin.balanceOf(this), decimalMultiplier.mul(60000000), "Incorrect.");
-    //    Assert.equal(qin.balanceOf(freeze), decimalMultiplier.mul(140000000), "Incorrect.");
-    //}
+        Assert.isFalse(freeze.frozen(), "Frozen before payment.");
+        Assert.isTrue(qin.transfer(freeze, decimalMultiplier.mul(140000000)), "Transfer to freeze failed.");
+        Assert.isTrue(freeze.frozen(), "Not frozen.");
+        Assert.equal(qin.balanceOf(this), decimalMultiplier.mul(60000000), "Incorrect.");
+        Assert.equal(qin.balanceOf(freeze), decimalMultiplier.mul(140000000), "Incorrect.");
+    }
 
-    //function testQINFrozenOwner() {
-    //    uint releaseTime = now + 1000;
-    //    QINToken qin = new QINToken(true);
-    //    QINFrozen freeze = new QINFrozen(qin, releaseTime);
+    function testQINFrozenOwner() {
+        uint releaseTime = now + 1000;
+        QINToken qin = new QINToken(true);
+        QINFrozen freeze = new QINFrozen(qin, releaseTime, true);
 
-    //    Assert.equal(freeze.owner(), this, "Not the correct owner. ");
-    //}
+        Assert.equal(freeze.owner(), this, "Not the correct owner. ");
+    }
 
-    //function testQINFrozenSupportsToken() {
-    //    uint releaseTime = now + 1000;
-    //    QINToken qin = new QINToken(true);
-    //    QINFrozen freeze = new QINFrozen(qin, releaseTime);
+    function testQINFrozenSupportsToken() {
+        uint releaseTime = now + 1000;
+        QINToken qin = new QINToken(true);
+        QINFrozen freeze = new QINFrozen(qin, releaseTime, true);
 
-    //    bool support = freeze.supportsToken(qin);
+        bool support = freeze.supportsToken(qin);
 
-    //    Assert.equal(support, true, "supportsToken() is rejecting QIN.");
-    //}
+        Assert.equal(support, true, "supportsToken() is rejecting QIN.");
+    }
 
     // TODO(thiefinparis): Like the tests at the top, triggering the release will require hooks into
     // the internal contract state to modify the time to allow the payment and release to occur in
     // the same solidity transaction.
 
-    //function testReleaseFunction() {
-    //    uint releaseTime = now + 1000;
-    //    uint frozenBalance = 200000000;
-    //    QINToken qin = new QINToken(true);
-    //    QINFrozen freeze = new QINFrozen(qin, releaseTime);
+    function testReleaseFunction() {
+        uint releaseTime = now + 1000;
+        QINToken qin = new QINToken(true);
+        QINFrozen freeze = new QINFrozen(qin, releaseTime, true);
 
-    //    qin.transfer(freeze, 140000000);
+        qin.transfer(freeze, 140000000);
 
-    //    freeze.release(msg.sender);
-    //    Assert.equal(qin.balanceOf(freeze), 0, "Frozen balance was not reset.");
+        freeze.setCurrentTime(now + 1001);
+        freeze.release(msg.sender);
+        Assert.equal(qin.balanceOf(freeze), 0, "Frozen balance was not reset.");
 
-    //}
+    }
 
 }
