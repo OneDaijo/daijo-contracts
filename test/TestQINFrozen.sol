ea6dd237e6553e9938128f53fefccb743ae57b94
Matt Rice <thiefinparis@users.noreply.github.com>
2017-10-07 19:26:11 -0400

    Minor cleanup for tests using isTrue() and isFalse() (#59)

--- test/TestQINFrozen.sol
+++ test/TestQINFrozen.sol
@@ -78,7 +78,7 @@ contract TestQINFrozen {
 
         bool support = freeze.supportsToken(qin);
 
-        Assert.equal(support, true, "supportsToken() is rejecting QIN.");
+        Assert.isTrue(support, "supportsToken() is rejecting QIN.");
     }
 
     // TODO(thiefinparis): Like the tests at the top, triggering the release will require hooks into
