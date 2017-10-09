c096081df064d5bc1f80e6433ab4ac43b9f19bbb
Matt <mrice@worldrapidfinance.com>
2017-10-08 21:39:26 -0400

    Safe math for uint8 operations (#63)

--- contracts/libs/SafeMath256.sol
+++ contracts/libs/SafeMath256.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.4.13;
  /** @title SafeMath Library
  *  @author WorldRapidFinance <info@worldrapidfinance.com>
  *  @notice source: OpenZeppelin
- *  @dev math operations with safety checks that throw on error
+ *  @dev math operations with safety checks that throw on error for uint
  */
-library SafeMath {
+library SafeMath256 {
     function mul(uint a, uint b) internal constant returns (uint) {
         uint c = a * b;
         assert(a == 0 || c / a == b);
@@ -29,5 +29,11 @@ library SafeMath {
         uint c = a + b;
         assert(c >= a);
         return c;
+    }
+
+    function castToUint8(uint a) internal constant returns (uint8) {
+        uint8 b = uint8(a);
+        assert(a == b);
+        return b;
     }
 }
